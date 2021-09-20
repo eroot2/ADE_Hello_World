e4f96e5e4f3917560074299ddc78beab149b72ad
eroot2 <eroot2@wisc.edu>
2021-09-20 04:45:49 -0500

    ohhh i found the comment area

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imgview; //comment for test push
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
